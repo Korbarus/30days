1ea300f466d738fbf1ff8e6c9df918d6554398d1
Korbarus <korbarus2951@gmail.com>
2024-07-08 13:49:23 +0100

    Day 13

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -2,13 +2,14 @@
     <x-slot:heading>
         about
     </x-slot:heading>
-    <ul>
+    <div class="space-y-4">
     @foreach ($jobs as $job)
-        <li>
-            <a href="jobs/{{$job['id']}}">
-            <strong>{{$job['title']}}</strong> pays {{$job['salary']}}
+            <a href="jobs/{{$job['id']}}" class="block px-4 py-6 border border-gray-200">
+                <div class="font-bold text-blue-500 text-sm">{{$job->employer->name}}</div>
+                <div>
+                    <strong>{{$job['title']}}</strong> pays {{$job['salary']}}
+                </div>
             </a>
-        </li>
-    </ul>
     @endforeach
+    </div>
 </x-layout>
